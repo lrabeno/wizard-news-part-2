f7b4cec40d1f7bf7e82a23e52c42af8db47b47b7
Louis Rabeno <lrabeno@gmail.com>
2022-09-15 21:27:23 -0400

    fixed injection bug

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const postList = require("./views/postList");
 const postDetails = require("./views/postDetails");
 const client = require("./db/index");
 
+// console.log("CLIENT ----------------->", client);
+
 const app = express();
 
 app.use(morgan("dev"));
@@ -23,10 +25,14 @@ app.get("/", async (req, res, next) => {
 });
 
 app.get("/posts/:id", async (req, res) => {
-  const data = await client.query(
-    `SELECT * FROM posts WHERE ID =${req.params.id}`
-  );
+  const data = await client.query(`SELECT * FROM posts WHERE ID =$1`, [
+    req.params.id,
+  ]);
+
   const [post] = data.rows;
+
+  // without destructuring
+  // const post = data.rows[0];
   res.send(postDetails(post));
   console.log(post);
   // const data = await client.query("SELECT * FROM posts");
